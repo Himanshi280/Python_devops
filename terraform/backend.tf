db1eea191f711b3c0201c4e180f941486d635f72
Himanshi280 <84514251+Himanshi280@users.noreply.github.com>
2025-05-25 22:23:44 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "python-0125" # Replace with your bucket name
+    bucket         = "statefilebackup99" # Replace with your bucket name
     key            = "terraform.tfstate"
     region         = "us-east-1"
   }
