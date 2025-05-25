49a58bbcf741ac151ea24f76c8493a6feb8efd66
Himanshi280 <84514251+Himanshi280@users.noreply.github.com>
2025-05-25 21:13:30 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami                     = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
-
-
+  security_groups =       [ "launch-wizard-2" ]
+  tags = {
+    Name = "Python"
+  }
 }
